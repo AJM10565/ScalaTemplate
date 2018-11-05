002d20c2d06a8d0c09aa9503ddc9dcfa0fa3d192
msukan <31421218+msukan@users.noreply.github.com>
2018-11-05 00:48:32 -0600

    Updated more TestFixtures

--- src/test/scala/TestFixtures.scala
+++ src/test/scala/TestFixtures.scala
@@ -1,4 +1,4 @@
-package edu.luc.cs.laufer.cs473.expressions
+ package edu.luc.cs.laufer.cs473.expressions
 
 object TestFixtures {
 
@@ -24,14 +24,6 @@ object TestFixtures {
     Constant(5)
   )
 
-  //val simple4string = "x = ((1 + y2) - (3 * y4)) / 5;"
-  //val simple5string = "if (1) { x = 2; }"
-  //val simple6string = "if (1) { x = 2; } else { x = 3; }"
-  //val simple7string = "{ r = r + x; y = y + 1 ; }"
-  val simple8string = "if (4) { r = r + x; y = y + 1; }"
-  val simple9string = "while (y) { r = r + x; y = y - 1; }"
-  val simple10string = "while (y) { r = r + x ; y = y - 1 ;}"
-
   val simple4string = "x = ((1 + y2) - (3 * y4)) / 5;"
   val simple4 =
     Assignment(
@@ -52,16 +44,95 @@ object TestFixtures {
     )
 
   val simple5string = "if (1) { x = 2; }"
-  //val simple5 =
-  //       Conditional(1, Expr, Block(Assignment(Constant(2)))
+  val simple5 =
+         Conditional(Constant(1),
+          Block(
+           Assignment(
+             Variable("x"),
+              Constant(2)
+           )
+         )
+       )
 
   val simple6string = "if (1) { x = 2; } else { x = 3; }"
-  //val simple6 =
-  //    //  Conditional(1, Expr, Block(
+   val simple6 =
+         TrinaryExpr(
+           Constant(1),
+           Block(
+            Assignment(
+            Variable("x"),
+            Constant(2))
+           ),
+           Block(
+             Assignment(
+             Variable("x"),
+             Constant(3))
+           )
+          )
 
   val simple7string = "{ r = r + x; y = y + 1 ; }"
-  //  val simple7 =
-  //      //Assignment(Variable("r"), )
+  val simple7 =
+          Block(
+            Assignment(
+              Variable("r"),
+               Plus(
+                 Variable("r"),
+                 Variable("x")
+           )
+         )
+           Assignment(
+             Variable("y"),
+              Plus(
+                Variable("y"),
+                Constant(1))
+           )
+         )
+
+  val simple8string = "if (4) { r = r + x; y = y + 1; }"
+  val simple8 =
+         Conditional(
+           Constant(4),
+            Block(
+              Assignment(
+              Variable("r"),
+               Plus(
+                 Variable("r"),
+                 Variable("x")
+            )
+          )
+      Assignment(
+        Variable("y"),
+         Plus(
+          Variable("y"),
+           Constant(1))
+        )
+       )
+      )
+
+  val simple9string = "while (y) { r = r + x; y = y - 1; }"
+  val simple9 =
+       Loop(
+        Variable("y"),
+         Block(
+          Assignment(
+           Variable("r"),
+            Plus(
+              Variable("r"),
+              Variable("x")
+            )
+          )
+              Assignment(
+               Variable("y"),
+                Minus(
+                  Variable("y"),
+                  Constant(1)
+                )
+              )
+            )
+          )
+
+//val simple10string = "while (y) { r = r + x ; y = y - 1 ;}"
+//val simple10 =
 
   val complex1string = "((1 + 2) - (3 * 4)) / 5"
   val complex1 =
